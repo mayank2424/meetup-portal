a2164fb8be7d00d15269c5d687761e8ab44fed77
mayank <mayankgupta2424@gmail.com>
2017-08-04 16:27:18 +0530

    testing oauth command

--- assets/main.js
+++ assets/main.js
@@ -5,4 +5,18 @@ $(document).ready(function(){
     });
 });
 
-var username= 
+var username= $('.form-input').val();
+var password=$('.password').val();
+
+var authServerUri =" https://api.meetup.com/oauth/access/",
+authParams = {
+  response_type: "GET",
+  client_id: this.model.get("70aj8rnkrnqk6i7m3ptal48cps"),
+  redirect_uri: this.model.get("https://mayank2424.github.io/meetup-portal/"),
+//   scope: this.model.get("scope"),
+//   state: this.model.get("state")
+};
+
+// Redirect to Authorization page.
+var replacementUri = authServerUri + "?" + $.param(authParams);
+window.location.replace(replacementUri);
